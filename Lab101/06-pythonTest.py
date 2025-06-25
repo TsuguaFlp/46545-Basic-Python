d2e73444faa29270494e6405a8e91507c7921b92
TsuguaFlp <wbyte4u@gmail.com>
2025-06-25 09:44:16 +0700

    New Test

---
+++ Lab101/06-pythonTest.py
@@ -0,0 +1,7 @@
+score = int(input('Enter your score: '))
+if score <= 10:
+    print('ไม่ผ่าน')
+elif score <= 20:
+    print('ปรับปรุง')
+elif score <= 30:
+    print('ดีมาก')
---
+++ Lab101/07-PythonTest.py
@@ -0,0 +1,5 @@
+char = str(input("Ente char: "))
+if char == 'A':
+    print('Is A')
+else:
+    print('Not A')
