3352f03b5737bb4de416673baff6d47a42e02bb3
TsuguaFlp <wbyte4u@gmail.com>
2025-07-23 09:50:04 +0700

    งานวันนี้

---
+++ Lab101/09-Pythonloop.py
@@ -0,0 +1,3 @@
+m = int(input("กรุณาใส่แม่ที่ต้องการ: ")) 
+for j in range(1, 13):
+    print(f'{m} x {j} = {m*j}')
---
+++ Work/PythonLoop/06-sum_odd.py
@@ -0,0 +1,3 @@
+n = int(input())
+total = sum(i for i in range(1, n + 1, 2))
+print(total)
